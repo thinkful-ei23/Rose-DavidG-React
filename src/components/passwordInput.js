b07c045754e9641bbbe6db9b0de15ff3126df282
David Graves <davidgraves831@gmail.com>
2018-08-29 08:37:27 -0700

    added passwordInput

---
+++ src/components/passwordInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PasswordInput(props){
+    return (
+        <form> 
+            <label>Password Input </label>
+            <input type="text"/>
+            </form>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import PasswordInput from './components/passwordInput';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<PasswordInput />, document.getElementById('root'));
 registerServiceWorker();

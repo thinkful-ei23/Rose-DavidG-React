52982765a76e37ae38aefc7cab1f4f8638408f20
David Graves <davidgraves831@gmail.com>
2018-08-29 09:07:32 -0700

    added some password validation

--- src/components/validate.js
+++ src/components/validate.js
@@ -1,1 +1,33 @@
 import React from 'react';
+
+export default function Validate(password){
+    const check=password;
+        const sizedFields = {
+            password: { min: 8, max: 72 }
+          };
+        
+          const tooSmallField = Object.keys(sizedFields).find(
+            field => 'min' in sizedFields[field] &&
+              req.body[field].trim().length < sizedFields[field].min
+          );
+          if (tooSmallField) {
+            const min = sizedFields[tooSmallField].min;
+            const err = new Error(`Field: '${tooSmallField}' must be at least ${min} characters long`);
+            err.status = 422;
+            return next(err);
+          }
+        
+          const tooLargeField = Object.keys(sizedFields).find(
+            field => 'max' in sizedFields[field] &&
+              req.body[field].trim().length > sizedFields[field].max
+          );
+        
+          if (tooLargeField) {
+            const max = sizedFields[tooLargeField].max;
+            const err = new Error(`Field: '${tooLargeField}' must be at most ${max} characters long`);
+            err.status = 422;
+            return next(err);
+          }
+
+          return check;
+    }
